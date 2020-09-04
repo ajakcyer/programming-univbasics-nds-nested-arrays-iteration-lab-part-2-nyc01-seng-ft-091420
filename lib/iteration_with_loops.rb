072dd4c39b094f73240404d6c74453d4f6bf5e63
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-04 19:34:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,14 +1,22 @@
 def find_min_in_nested_arrays(src)
   count = 0
+  
   new_array = []
   while count < src.length do
+    
     inner_count = 0
-    min_value = 100
+    min_value = 200
     while inner_count < src[count].length do
-      if min_value < src[count][inner_count]
+      if min_value > src[count][inner_count]
+        min_value = src[count][inner_count]
+      end
+    
+      inner_count += 1
     end
+    new_array.push(min_value)
     inner_count += 1
   end
+  new_array
 end
 
   # src will be an array of arrays of integers
